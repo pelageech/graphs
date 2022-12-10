09705747c76044faecc4447c0679bf63b8dc4468
pelageech <va.oleg2015@yandex.ru>
2022-12-10 10:10:43 +0700

    graph shows in stdout

--- app/Main.hs
+++ app/Main.hs
@@ -7,7 +7,7 @@ import GraphicsSVG
     ( allFunction, Graph(segment, width, height, xstart, ystart) )
 
 import qualified Data.Text as T
-import Data.Text.IO as T.IO ( writeFile )
+import Data.Text.IO as T.IO ( putStr )
 
 import ParserSVG ( parse )
 
@@ -21,6 +21,6 @@ checkedValues graph | segment graph > 500 || segment graph < 20                 
                     | width graph < segment graph || height graph < segment graph = print "Error: Wrong window size: width and height must be greater than a single line segment"
                     | width graph > 5000 || height graph > 3000                   = print "Error: Wrong window size: width and height must be less than 5000"
                     | xstart graph < 0 || ystart graph < 0                        = print "Error: Screen start point set incorrectly"
-                    | otherwise                                                   = T.IO.writeFile "output.svg" (createSVG w h $ allFunction graph) where
+                    | otherwise                                                   = T.IO.putStr (createSVG w h $ allFunction graph) where
                                                                                         w = width graph + 2 * xstart graph
                                                                                         h = height graph + 2 * ystart graph
